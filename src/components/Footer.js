e4f0985510c9a7264f64d3f9fb14dbe3c6d8c273
toshittandon <toshittandon9@gmail.com>
2025-09-07 18:33:59 +0200

    V10.2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaTwitter, FaHeart } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
